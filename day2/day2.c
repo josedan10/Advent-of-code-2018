65a351c6041b5353efb05dc54c07d8ab5cb978ff
Jose Daniel <josedanq100@gmail.com>
2018-12-31 17:59:49 -0400

    Solved day 2

--- day2/day2.c
+++ day2/day2.c
@@ -13,8 +13,6 @@ void validateWord(char *str, int *check3, int *check2) {
     int i, j, n = 0, count, check2Already = 0, check3Already = 0;
     char table[27] = "";
 
-    printf("\nMy cadena: %s\n", str);
-
     for (i = 0; i < strlen(str) - 1; i++) {
         
         if (isNotVerified(table, str[i], n)) {
@@ -29,7 +27,6 @@ void validateWord(char *str, int *check3, int *check2) {
                     if (!check2Already){
                         *check2 += 1;
                         check2Already = 1;
-                        printf("Se repite %c dos veces.\n", str[i]);
                     }
                     break;
                 
@@ -37,7 +34,6 @@ void validateWord(char *str, int *check3, int *check2) {
                     if (!check3Already){
                         *check3 += 1;
                         check3Already = 1;
-                        printf("Se repite %c tres veces.\n", str[i]);
                     }
                     break;
                 
@@ -50,25 +46,129 @@ void validateWord(char *str, int *check3, int *check2) {
     }
 }
 
-int main()
-{
-    int checkTwo = 0, checkThree = 0, n = 0;
-    char str[30];
+typedef struct node {
+    char text[30];
+    struct node *next;
+} Node;
+
+typedef struct list {
+    Node *root;
+    unsigned short length;
+} List;
+
+// List constructor
+
+List* newList() {
+    List *list = (List *) malloc(sizeof(List));
+    list->root = NULL;
+    list->length = 0; 
+    return list;
+}
+
+void addToList(List *list, char *str) {
+    Node *node = (Node *) malloc(sizeof(Node));
+    strcpy(node->text, str);
+    node->next = NULL;
+    
+    Node *auxList = list->root;
+    
+    if (list->root == NULL) list->root = node;
+    else {
+        while (auxList->next != NULL) auxList = auxList->next;
+        auxList->next = node;
+    }
+
+    list->length++;
+}
+
+void removeFromList(List *list, Node *node) {
+    Node *auxNode = list->root;
+
+    if (!list->length) return; // La lista está vacía
+
+    if (auxNode = node) {
+        list->root = auxNode->next;
+        free(node);
+    } else {
+        while (auxNode->next != node) auxNode = auxNode->next;
+        auxNode->next = node->next;
+
+        free(node);
+    }
+
+    list->length--;
+}
+
+/*
+    Description: this function return true if only ONE character is different comparing 2 strings
+    @params:
+    char *str1, String 1 different of NULL
+    char *str2, String 2 different of NULL
+    int len     Length of strings
+*/
+
+int compareStrings(char *str1, char *str2, int len) {
+    int counter = 0;
+
+    for (int i = 0; i < len; i++) {
+        if (str1[i] != str2[i]) counter++;
+    }
+
+    return counter == 1;
+}
+
+void printCoincidence(char *str1, char *str2) {
+    unsigned short len = strlen(str1), i;
+    printf("Frase de coincidencia: \n");
+    for (i = 0; i < len; i++)
+        if (str1[i] == str2[i]) printf("%c", str1[i]);
+        
+    printf("\n");
+}
+
+int main() {
+    int checkTwo = 0, checkThree = 0, n = 0, i;
+    char str[30], auxStr[30];
+    Node *auxCompNode;
+    List *list = newList();
     FILE * fp;
 
     fp = fopen("day2/input.txt", "r");
 
     if (fp != NULL) {
         while(!feof(fp)) {
-            printf("\n===================Fila: %d\n", ++n);
             fscanf(fp, "%s", str);
-            validateWord(str, &checkThree, &checkTwo);
+            addToList(list, str);
+            n++;
+            // validateWord(str, &checkThree, &checkTwo);
+            // free(str);
         }
     }
 
-    printf("\nEn total se repiten %d letras dos veces y %d letras 3 veces. La checksum es %d\n\n", checkTwo, checkThree, checkThree * checkTwo);
+    printf("Escaneados un total de %d strings\n\n", n);
+
+    // printf("\nEn total se repiten %d letras dos veces y %d letras 3 veces. La checksum es %d\n\n", checkTwo, checkThree, checkThree * checkTwo);
 
     fclose(fp);
+
+    while (list->length > 2) {
+        strcpy(auxStr, list->root->text);
+
+        auxCompNode = list->root->next;
+
+        while (auxCompNode != NULL && !compareStrings(auxStr, auxCompNode->text, strlen(auxStr))) auxCompNode = auxCompNode->next;
+
+        if (auxCompNode != NULL) {
+            // Se encontraron dos strings que difieren solo en un caracter
+            printCoincidence(auxCompNode->text, auxStr);
+            break;
+        } else {
+            removeFromList(list, list->root);
+        }
+
+    }
+
+    // printf("Strings finales: %s y %s\n", list->root->text, list->root->next->text);
 
     system("PAUSE");
 
